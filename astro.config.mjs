5c5165bdf35b70403e664bb89f1eb331b7824434
myrus1 <mendezfacundo80@gmail.com>
2026-03-05 11:28:49 -0300

    migracion Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://myrus1.github.io',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
